342026e411ebf643e9cf1ea6540c3c3cddb94928
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-01-31 12:46:40 +0100

    image index working

--- src/components/elements/Card.jsx
+++ src/components/elements/Card.jsx
@@ -13,7 +13,7 @@ export default function Card({ property }) {
       <Link className="text-decoration-none" to={`/properties/${property.id}`}>
         <div className="card h-100">
           <div>
-            <img src={property.image} className="img-fluid" />
+            <img src={property.image[0].img_url} className="img-fluid" />
           </div>
           <div className="card-body">
             <div className="row ">
