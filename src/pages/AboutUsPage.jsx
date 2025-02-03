039213a240ca2160958022d4de13cf639b890ac4
giuseppemsabino <giuseppemendoza004@gmail.com>
2025-02-03 17:18:43 +0100

    aboutPage fixed

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -5,44 +5,44 @@ export default function AboutUsPage() {
             <h1>Developers</h1>
         </div>
       <div className="d-flex justify-content-around us-section">
-        <div className="card team-card col-3">
+        <div className=" team-card col-3">
           <img
             src="src/assets/img/angelo.jpg"
             className="card-img-top"
             alt="..."
           />
           <div className="card-body text-center">
-            <h5 className="card-title">Angelo Gemelli</h5>
+            <h5 className="card-title mt-5"><strong> Angelo Gemelli </strong></h5>
           </div>
         </div>
-        <div className="card team-card col-3">
+        <div className=" team-card col-3">
           <img
             src="src/assets/img/dario.jpg"
             className="card-img-top"
             alt="..."
           />
           <div className="card-body text-center">
-            <h5 className="card-title">Dario Miceli</h5>
+            <h5 className="card-title mt-5"><strong> Dario Miceli </strong></h5>
           </div>
         </div>
-        <div className="card team-card col-3">
+        <div className=" team-card col-3">
           <img
             src="src/assets/img/ema.jpg"
             className="card-img-top"
             alt="..."
           />
           <div className="card-body text-center">
-            <h5 className="card-title">Emanuele Zuppardo</h5>
+            <h5 className="card-title mt-5"><strong> Emanuele Zuppardo </strong></h5>
           </div>
         </div>
-        <div className="card team-card col-3">
+        <div className=" team-card col-3">
           <img
             src="src/assets/img/giuseppe.jpg"
             className="card-img-top"
             alt="..."
           />
           <div className="card-body text-center">
-            <h5 className="card-title">Giuseppe A. Mendoza S.</h5>
+            <h5 className="card-title mt-5"><strong> Giuseppe A. Mendoza S. </strong></h5>
           </div>
         </div>
       </div>
